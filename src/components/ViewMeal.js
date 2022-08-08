064b3dfe3d535dc76062e3d000fee5337d41b828
Bradley Hartlove <bradleyhartlovecodes@gmail.com>
2022-08-07 23:22:22 -0400

    Updates for week six

--- src/components/ViewMeal.js
+++ src/components/ViewMeal.js
@@ -33,40 +33,55 @@ const ViewMeal = () => {
   return (
     <>
     
-    <div class="grid-cols-3 grid">
-        <div class="border-2 border-lime-500 col-span-2">
-          <div class="border-2 border-blue-500 inline-block">
-            <div class="border-2 border-red-500 mt-4 mb-4 w-9/12 float-right mr-16">
+    {
+      !loading && (
+        <div class="grid-cols-3 grid">
+        <div class="  col-span-2">
+          <div class="  inline-block">
+            <div class="  mt-4 mb-4 w-9/12 float-right mr-16">
               <img src="/placeholder.jpg" alt="Meal"/>
             </div>
           </div>
           <div class="relative">
-            <div class="border-2 border-purple-500 inline-block w-full">
+            <div class="  inline-block w-full">
               <h4 class="text-3xl font-bold float-left ml-64">Description</h4>
             </div>
-            <div class="border-2 border-purple-500 inline-block w-full">
+            <div class="  inline-block w-full">
               <p class="text-lg float-left ml-64">{meal.description}</p>
             </div>
-            <div class="border-2 border-purple-500 inline-block w-full">
+            <div class="  inline-block w-full">
               <h4 class="text-3xl font-bold float-left ml-64">Recipe</h4>
             </div>
-            <div class="border-2 border-purple-500 inline-block w-full">
+            <div class="  inline-block w-full">
               <p class="text-lg float-left ml-64 text-left">{meal.recipe}</p>
             </div>
             </div>
         </div>
-        <div class="border-2 border-lime-500 ">
-          <div class="border-2 border-purple-500 inline-block w-full mt-4">
+        <div class="  ">
+          <div class="  inline-block w-full mt-4">
             <h4 class="text-4xl font-bold">{meal.name}</h4>
           </div>
-          <div class="border-2 border-purple-500 inline-block w-full mt-4">
+          <div class="  inline-block w-full mt-4">
             <h4 class="text-2xl">Poster: {meal.poster}</h4>
           </div>
-          <div class="border-2 border-purple-500 inline-block w-full mt-4">
+          <div class="  inline-block w-full mt-4">
             <h4 class="text-2xl">Likes: {meal.likes}</h4>
+          </div>
+          <div class="  inline-block w-full mt-4">
+            <h4 class="text-2xl">Tags:</h4>
+          </div>
+          <div class="  inline-block w-full mt-4">
+          {meal.tags.map((tag) => (
+          <div className="bg-lime-500  shadow-sm w-1/6 mx-auto my-2 text-white rounded-2xl inline-block">
+            {tag}
+          </div>
+          ))}
           </div>
         </div>
       </div>
+      
+      )
+    }
       
     </>
   )
